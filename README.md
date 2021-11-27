2d4f56fb442e3495eafb515ee3ee4256e8741055
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2021-11-27 18:45:41 +0200

    simple is better than complex, faster is better than nothing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # easy-queue
-ring buffer implementation in ansi c
+
+A simple ring buffer implementation in ANSI C
+
+
+## why you ask ?
+
+It is fully tested, easy to use, ready to build, smallest and also
+fast, considering the required null checks.
--- main_tests.cpp
+++ main_tests.cpp
@@ -15,20 +15,65 @@ extern "C" {
 TEST(InitQueue, NewQueueIsEmpty) {
     EasyQueue first;
     EasyQueue second;
+    int ok_count = 0;
 
-    zq_init( &first );
-    zq_init( &second );
+    ok_count += zq_init( &first );
+    ok_count += zq_init( &second );
+    ASSERT_EQ( ok_count, 2 );
 
-    EXPECT_TRUE( zq_is_empty( &first ) );
-    EXPECT_TRUE( zq_is_empty( &second ) );
-
+    ASSERT_TRUE( zq_is_empty( &first ) );
+    ASSERT_TRUE( zq_is_empty( &second ) );
 }
 
-TEST(PushQueue, AddElementIncreasesSize) {
-    EasyQueue q;
-
-
+TEST(InitQueue, UninitializedQueueIsNotEmpty) {
+    EasyQueue queue;
+    // Should not really rely on such test
+    ASSERT_FALSE( zq_is_empty( &queue ) );
 }
+
+TEST(PushQueue, WorksWhenEmpty) {
+    EasyQueue queue;
+    int ok_count = 0;
+    uint8_t value = 42;
+
+    ok_count += zq_init( &queue );
+    ok_count += zq_push( &queue, value );
+    ok_count += zq_push( &queue, value );
+    ok_count += zq_push( &queue, value );
+
+    ASSERT_EQ(ok_count, 4);
+}
+
+TEST(PushQueue, FailsWhenFull) {
+    EasyQueue queue;
+    int ok_count = 0;
+    uint8_t value = 42;
+
+    ok_count += zq_init( &queue );
+
+    queue.length = QUEUE_SIZE;  // consumers should never access struct members
+    ok_count += zq_push( &queue, value );
+    ok_count += zq_push( &queue, value );
+    ok_count += zq_push( &queue, value );
+
+    ASSERT_EQ(ok_count, 1);
+    ASSERT_EQ(queue.length, QUEUE_SIZE);
+}
+
+TEST(PopQueue, FailsWhenEmpty) {
+    EasyQueue queue;
+    int ok_count = 0;
+    uint8_t value = 42;
+
+    ok_count += zq_init( &queue );
+    ok_count += zq_pop( &queue, &value );
+    ok_count += zq_pop( &queue, &value );
+    ok_count += zq_pop( &queue, &value );
+
+    ASSERT_EQ(ok_count, 1);
+    ASSERT_EQ(value, 42);
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
